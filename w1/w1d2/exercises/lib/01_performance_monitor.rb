fab474304a714647c7835dbbd2fd81b352b9ed59
Alex Doundakov <adoundakov@gmail.com>
2016-06-16 15:09:26 -0700

    Update w1d2 exercises

--- w1/w1d2/exercises/lib/01_performance_monitor.rb
+++ w1/w1d2/exercises/lib/01_performance_monitor.rb
@@ -1,3 +1,17 @@
-def elapsed_time(&block)
-  block.call
+require 'Time'
+
+def measure(reps=1, &block)
+  elapsed_times = []
+  reps.times do
+      start = Time.now
+      block.call
+      elapsed_time = Time.now - start
+      elapsed_times << elapsed_time
+  end
+  average(elapsed_times).to_f
+end
+
+def average(times)
+    sum = times.inject(:+)
+    sum / times.length
 end
