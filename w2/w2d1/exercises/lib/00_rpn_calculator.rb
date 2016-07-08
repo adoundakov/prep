fdf0f6dff5aa4401107098e4c8790723f01b8f5d
Alex Doundakov <adoundakov@gmail.com>
2016-07-07 23:56:43 -0700

    Week 2 Progress

--- w2/w2d1/exercises/lib/00_rpn_calculator.rb
+++ w2/w2d1/exercises/lib/00_rpn_calculator.rb
@@ -1,3 +1,64 @@
 class RPNCalculator
-  # TODO: your code goes here!
+  attr_reader :store
+
+  def initialize
+    @store = []
+  end
+
+  def value
+    store.last unless empty?
+  end
+
+  def push(number)
+    @store << number
+  end
+
+  def plus
+    unless empty?
+      store << pop + pop
+    end
+  end
+
+  def minus
+    unless empty?
+      store << pop - pop
+    end
+  end
+
+  def times
+    unless empty?
+      store << pop * pop
+    end
+  end
+
+  def divide
+    unless empty?
+      store << pop / pop
+    end
+  end
+
+  def tokens(string)
+  end
+
+  def evaluate(string)
+  end
+
+  private
+
+  @operations = {'+' => :plus, '-' => :minus,
+                '*' => :times, '/' => :divide}
+
+  def empty?
+    if @store.empty?
+      raise Exception.new("calculator is empty")
+    end
+  end
+
+  def pop
+    if operations[value]
+      self.operations[value] # need to fix this function call
+    else
+      @store.pop
+    end
+  end
 end
