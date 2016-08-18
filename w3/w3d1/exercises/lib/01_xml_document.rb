e3872985c9dd89354e42fc4307e173e2aec5552b
Alex Doundakov <adoundakov@gmail.com>
2016-08-18 15:24:21 -0700

    XML Builder progress

--- w3/w3d1/exercises/lib/01_xml_document.rb
+++ w3/w3d1/exercises/lib/01_xml_document.rb
@@ -1,3 +1,13 @@
 class XmlDocument
-  # TODO: your code goes here!
+  attr_reader :tag
+
+  def initialize(indent = false)
+    @tag = ''
+  end
+
+  def method_missing(method_name, arg = nil)
+    if block_given?
+      # stuff
+    end
+  end
 end
