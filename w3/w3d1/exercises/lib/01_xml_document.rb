1b4d22a00a5b9de92be071c86940d41a40513af1
Alex Doundakov <adoundakov@gmail.com>
2016-08-23 16:28:08 -0700

    Finish XML Exercises

--- w3/w3d1/exercises/lib/01_xml_document.rb
+++ w3/w3d1/exercises/lib/01_xml_document.rb
@@ -1,13 +1,35 @@
 class XmlDocument
-  attr_reader :tag
+  attr_reader :ind_level, :indent
 
   def initialize(indent = false)
-    @tag = ''
+    @indent = indent
+    @ind_level = 0
   end
 
-  def method_missing(method_name, arg = nil)
-    if block_given?
-      # stuff
+  def method_missing(method_name, *args, &blk)
+    document = ''
+    attrs = args[0] || {} # since the args passed in are hashes
+    document << ('  ' * ind_level) if indent
+    document << "<#{method_name}"
+
+    attrs.each_pair do |k, v|
+      document << " #{k}=\"#{v}\""
     end
+
+    if blk
+      document << '>'
+      document << "\n" if indent
+      @ind_level += 1
+      document << yield
+      @ind_level -= 1
+      document << ("  " * ind_level) if indent
+      document << "</#{method_name}>"
+      document << "\n" if indent
+    else
+      document << "/>"
+      document << "\n" if indent
+    end
+
+    document
   end
 end
