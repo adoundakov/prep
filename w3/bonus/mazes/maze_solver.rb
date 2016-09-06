4926d4d8b8cacaa6632413b0f311a29959b5c976
Alex Doundakov <adoundakov@gmail.com>
2016-09-06 13:28:00 -0700

    Finish Maze Solver

--- w3/bonus/mazes/maze_solver.rb
+++ w3/bonus/mazes/maze_solver.rb
@@ -1,76 +1,107 @@
-require 'byebug'
+#!/usr/bin/env ruby
 
 class Maze
   def initialize(raw_maze)
+    @maze_name = raw_maze.delete('.txt')
     raw_lines = File.readlines(raw_maze)
     raw_lines.map! { |line| line.chomp.split('') }
 
-    @maze = raw_lines.map do |line|
-      line.map { |char| Square.new(char) }
+    @maze = Array.new(raw_lines.length) {Array.new}
+
+    raw_lines.each_with_index do |line, row|
+      line.each_with_index do |char, col|
+        @maze[row] << Square.new(char, [row,col])
+      end
     end
 
     @open_list = []
-    @closed_List = []
-    @current = {square: nil, pos: nil}
+    @closed_list = []
+    @current_square, @tgt_square = find('S'), find('E')
   end
 
   def solve
-    @current[:square], @current[:pos] = find_start
-  end
-
-  def find_start
-    debugger
-    
-    row = 0
-    @maze.each do |line|
-      col = 0
-      line.each do |square|
-        break if square.contents == 'S'
-        col += 1
-      end
-      row += 1
+    while true
+      add_adjacent
+      break if @open_list.include?(@tgt_square)
+      find_next
     end
 
-    pos = [row, col]
-    [self[*pos] , pos]
+    build_path
   end
 
-  def find_end
+  def find(char)
+    @maze.each do |line|
+      line.each do |square|
+        return square if square.contents == char
+      end
+    end
   end
 
   def find_next
+    scores = @open_list.map { |square| calc_F_score(square) }
+
+    next_pos = scores.index(scores.min)
+    next_square = @open_list[next_pos]
+    next_square.parent = @current_square
+
+    @closed_list << next_square
+    @open_list.delete(next_square)
+
+    @current_square = next_square
+    @open_list = []
+  end
+
+  def calc_F_score(square)
+    row, col = square.pos
+    tgt_row , tgt_col = @tgt_square.pos
+    square.f_score = (tgt_row - row).abs + (tgt_col - col).abs
   end
 
   def add_adjacent
-  end
-
-  def [](row, col)
-    @maze[row][col]
-  end
-
-# delete this after done with program? or could use it to write to file?
-  def display
-    maze.each do |line|
-      line.each do |square|
-        print square.contents
+    row, col = @current_square.pos
+    possible_moves = [ @maze[row+1][col],
+                       @maze[row-1][col],
+                       @maze[row][col-1],
+                       @maze[row][col+1]
+                      ]
+    possible_moves.each do |square|
+      unless square.contents == '*' || @closed_list.include?(square)
+        @open_list << square
       end
-      print "\n"
+    end
+  end
+
+  def build_path
+    @closed_list.each {|square| square.mark}
+
+    solved_maze = @maze.map do |line|
+      line.map { |square| square.contents }
+    end
+
+    File.open("#{@maze_name}_solution.txt", 'w') do |file|
+      solved_maze.each do |line|
+        file.puts line.join
+      end
     end
   end
 end
 
 class Square
-  attr_reader :parent, :contents, :f_score
+  attr_reader :contents, :pos
+  attr_accessor :f_score, :parent
 
-  def initialize(contents = nil)
+  def initialize(contents = nil, pos = [])
     @contents = contents
     @parent = nil
     @f_score = 0
+    @pos = pos
+  end
+
+  def mark
+    @contents = 'X'
   end
 end
 
-# if __FILE__ == 'maze.rb'
-#   file = ARGV[0]
-#   maze = Maze.new(file)
-#   maze.solve
-# end
+if __FILE__ == $0
+  Maze.new(ARGV[0]).solve
+end
